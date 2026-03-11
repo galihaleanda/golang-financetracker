218c2b91193da917b5651948f3543ffb21aaa421
Galih Aleanda <galihaleandaaa@gmail.com>
2026-03-11 14:26:28 +0700

    Adding User Models

--- models/user.go
+++ models/user.go
@@ -1,1 +1,37 @@
 package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        uint           `gorm:"primaryKey;autoIncrement" json:"id"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+
+	Name         string `gorm:"not null" json:"name"`
+	Email        string `gorm:"uniqueIndex;not null" json:"email"`
+	Password     string `gorm:"not null" json:"-"`
+	Avatar       string `json:"avatar"`
+	GoogleID     string `gorm:"uniqueIndex" json:"-"`
+	IsGoogleAuth bool   `gorm:"default:false" json:"is_google_auth"`
+
+	// Relationship
+	Categories []Category `gorm:"foreignKey:UserID"
+	json:"categories,omitempty"`
+
+	Transactions []Transaction `gorm:"foreignKey:UserID"
+	json:"transactions,omitempty"`
+}
+
+// For Api Response with no password
+type UserResponse struct {
+	ID           uint   `json:"id"`
+	Name         string `json:"name"`
+	Email        string `json:"email"`
+	Avatar       string `json:"avatar"`
+	IsGoogleAuth bool   `json:"is_google_auth"`
+}
