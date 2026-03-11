6c09b65d3d1e69bc79c1ba3a31ee56ebb728afec
Galih Aleanda <galihaleandaaa@gmail.com>
2026-03-11 14:00:34 +0700

    Adding Database Connection & Migration

--- config/database.go
+++ config/database.go
@@ -1,1 +1,47 @@
 package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_SSLMODE"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database", err)
+	}
+
+	log.Println("Database connected successfully")
+
+	err = db.AutoMigrate(
+		&models.User{},
+		&models.Category{},
+		&models.Transaction{},
+	)
+
+	if err != nil {
+		log.Fatal("Migration failed:", err)
+	}
+
+	DB = db
+}
