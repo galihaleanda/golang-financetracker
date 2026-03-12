6f86ad41bac6aaf316a1b93d77e5ed8ebbe38abb
valentineeeeee <galihaleandaaa@gmail.com>
2026-03-12 12:30:39 +0700

    Adding JWT Middleware

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,1 +1,39 @@
 package middleware
+
+import (
+	"finance-tracker/utils"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func (c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			utils.ErrorResponse(c, 401, "Authorization header required")
+			c.Abort()
+			return
+		}
+
+		// Bearer token
+		parts := strings.SplitN(authHeader, "", 2)
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			utils.ErrorResponse(c, 401, "Invalid or expired token")
+			c.Abort()
+			return
+		}
+
+		claims, err := utils.ValidateToken(parts[1])
+		if err != nil {
+			utils.ErrorResponse(c, 401, "Invalid or expired token")
+			c.Abort()
+			return
+		}
+
+		// Save user info to context for handler use
+		c.Set("userID", claims.UserID)
+		c.Set("userEmail", claims.Email)
+		c.Next()
+	}
+}
