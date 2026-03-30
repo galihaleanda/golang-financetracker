c08aeb0640d39669ef403a21e17c24ca1fb90467
Galih Aleanda <galihaleandaaa@gmail.com>
2026-03-30 11:56:52 +0700

    Completed - Adding Utils

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,1 +1,57 @@
 package utils
+
+import (
+	"errors"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type JWTClaims struct {
+	UserID uint   `json:"user_id"`
+	Email  string `json:"email"`
+	jwt.RegisteredClaims
+}
+
+// Creating new JWT Token
+func GenerateToken(userID uint, email string) (string, error) {
+	expiryHours, _ := strconv.Atoi(os.Getenv("JWT_EXPIRY_HOURS"))
+	if expiryHours == 0 {
+		expiryHours = 24
+	}
+
+	claims := JWTClaims{
+		UserID: userID,
+		Email:  email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Duration(expiryHours) * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+}
+
+// Validasi dan Parsing Token
+func validateToken(tokenString string) (*JWTClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &JWTClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, errors.New("Unexpected Signing Method")
+			}
+			return []byte(os.Getenv("JWT_SECRET")), nil
+		},
+	)
+
+	if err != nil || !token.Valid {
+		return nil, errors.New("Token invalid or expired soon")
+	}
+
+	claims, ok := token.Claims.(*JWTClaims)
+	if !ok {
+		return nil, errors.New("Invalid token claims")
+	}
+	return claims, nil
+}
--- utils/response.go
+++ utils/response.go
@@ -1,1 +1,28 @@
 package utils
+
+import "github.com/gin-gonic/gin"
+
+type APIResponse struct {
+	Success bool        `json:"success"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+	Error   string      `json:"error,omitempty"`
+}
+
+// Success dengan respons data
+func SuccessResponse(c *gin.Context, statusCode int, message string, data interface{}) {
+	c.JSON(statusCode, APIResponse{
+		Success: true,
+		Message: message,
+		Data:    data,
+	})
+}
+
+// Error Response
+func ErrorResponse(c *gin.Context, err error) {
+	c.JSON(400, APIResponse{
+		Success: false,
+		Message: "Validation Failed",
+		Error:   err.Error(),
+	})
+}
